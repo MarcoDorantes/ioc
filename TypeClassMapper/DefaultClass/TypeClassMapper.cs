7c3cdba505f025d669337a581f8d9dcae93314f6
Marco Dorantes <marcodorantes@hotmail.com>
2016-07-28 03:42:53 -0500

    Added file for TypeClassID

--- TypeClassMapper/DefaultClass/TypeClassMapper.cs
+++ TypeClassMapper/DefaultClass/TypeClassMapper.cs
@@ -4,51 +4,6 @@ using System.Collections.Generic;
 
 namespace nutility
 {
-  public class TypeClassID
-  {
-    public static implicit operator TypeClassID(string name) => new TypeClassID(name);
-
-    public TypeClassID(string id)
-    {
-      ID = id;
-    }
-
-    public string ID { get; private set; }
-
-    public override bool Equals(object obj)
-    {
-      if (obj == null)
-      {
-        return false;
-      }
-
-      TypeClassID other = obj as TypeClassID;
-      if (other == null)
-      {
-        return false;
-      }
-
-      return this.ID == other.ID;
-    }
-    public override int GetHashCode()
-    {
-      return this.ID != null ? this.ID.GetHashCode() : 0;
-    }
-    public override string ToString()
-    {
-      return ID;
-    }
-    public bool Equals(TypeClassID other)
-    {
-      if (other == null)
-      {
-        return false;
-      }
-
-      return this.ID == other.ID;
-    }
-  }
-
   /// <summary>
   /// Given the runtime dependency management tradition of early design patterns, e.g., Microsoft COM IUnknown::QueryInterface method, this class follows
   /// such design tradition and relies on basic equivalent mechanisms from .NET Framework (System.IServiceProvider interface).
