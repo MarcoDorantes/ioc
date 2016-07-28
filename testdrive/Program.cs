b4c096ad70a5c30c1873daf16a17808add53acff
marcodorantes <marcodorantes@hotmail.com>
2016-07-28 12:45:55 -0500

    Added testdrive.BootstrapShimUseCase.Run()

--- testdrive/Program.cs
+++ testdrive/Program.cs
@@ -3,6 +3,7 @@
   static void Main()
   {
     //testdrive.InitialUseCases.Run();
-    samplecase.SampleCase1.Run();
+    //samplecase.SampleCase1.Run();
+    testdrive.BootstrapShimUseCase.Run();
   }
 }
