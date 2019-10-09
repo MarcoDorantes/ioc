e3eba63331ddfe557ddba9ec6a0293e9d2fcf4b2
marcodorantes <marcodorantes@hotmail.com>
2019-10-08 23:29:56 -0500

    Added NETCoreRuntime\app1.csproj

---
+++ source/NETCoreRuntime/app1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+using static System.Console;
+
+namespace impl
+{
+  public class Handler : app1.IHandler
+  {
+    public void Show(TextWriter w)
+    {
+      w.WriteLine(GetType().FullName);
+    }
+  }
+}
+
+namespace app1
+{
+  public interface IHandler
+  {
+    void Show(TextWriter w);
+  }
+  class Program
+  {
+    static IHandler create(IServiceProvider typemap) => typemap.GetService(typeof(IHandler)) as IHandler;
+    static void Main(string[] args)
+    {
+      //WriteLine($"Config:{ConfigurationManager.AppSettings["A"]}");
+      WriteLine("Start");
+      IHandler h = create(new nutility.TypeClassMapper());
+      h.Show(Out);
+    }
+  }
+}
